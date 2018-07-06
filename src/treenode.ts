c233bfd99e790c9ad8db85394881720f1fdf479f
hackape <muse.vincent@gmail.com>
2018-07-06 11:58:25 +0800

    prettier code, add `addTreeNodeAdministration()` helper

--- src/treenode.ts
+++ src/treenode.ts
@@ -1,69 +1,113 @@
-import { observable, computed } from "mobx"
+import { observable, computed, action } from "mobx";
 
-export const $treenode = Symbol('$treenode')
+export const $treenode = Symbol("$treenode");
 
 export interface INode {
-    storedValue: any
-    subpath: string
-    readonly path: string
-    readonly parent: INode
-    readonly isRoot: boolean
-    readonly root: INode
+  storedValue: any;
+  subpath: string;
+  readonly path: string;
+  readonly parent: INode;
+  readonly isRoot: boolean;
+  readonly root: INode;
 }
 
-let nextNodeId = 1
+let nextNodeId = 1;
 export class TreeNode implements INode {
-    nodeId = ++nextNodeId
-    storedValue
-    @observable subpath = ""
-    @observable _parent = null as any as INode
+  nodeId = ++nextNodeId;
+  storedValue;
+  @observable subpath = "";
+  @observable _parent = (null as any) as INode;
 
-    @computed
-    get isRoot(): boolean {
-        return this.parent === null
+  @computed
+  get isRoot(): boolean {
+    return this.parent === null;
+  }
+
+  @computed
+  get parent(): INode {
+    return this._parent;
+  }
+
+  @action
+  setParent(newParent: INode, subpath: string | null = null) {
+    if (this.parent === newParent && this.subpath === subpath) return;
+    if (newParent) {
+      if (this._parent && newParent !== this._parent) {
+        throw new Error(
+          `A node cannot exists twice in the state tree. Failed to add ${this} to path '${
+            newParent.path
+          }/${subpath}'.`
+        );
+      }
+      if (!this._parent && newParent.root === this) {
+        throw new Error(
+          `A state tree is not allowed to contain itself. Cannot assign ${this} to path '${
+            newParent.path
+          }/${subpath}'`
+        );
+      }
     }
 
-    @computed
-    get parent(): INode {
-        return this._parent
+    if (!this.parent && !newParent) {
+      // noop
+    } else {
+      this.subpath = subpath || "";
+      if (newParent && newParent !== this._parent) {
+        this._parent = newParent;
+      }
     }
+  }
 
-    setParent(newParent: INode, subpath: string | null = null) {
-        if (this.parent === newParent && this.subpath === subpath) return
-        if (newParent) {
-            if (this._parent && newParent !== this._parent) {
-                throw new Error(
-                    `A node cannot exists twice in the state tree. Failed to add ${this} to path '${newParent.path}/${subpath}'.`
-                )
-            }
-            if (!this._parent && newParent.root === this) {
-                throw new Error(
-                    `A state tree is not allowed to contain itself. Cannot assign ${this} to path '${newParent.path}/${subpath}'`
-                )
-            }
-        }
+  @action
+  setSubpath(subpath: string) {
+    this.subpath = subpath;
+  }
 
-        if (!this.parent && !newParent) {
-            // noop
-        } else {
-            this.subpath = subpath || ""
-            if (newParent && newParent !== this._parent) {
-                this._parent = newParent
-            }
-        }
+  @computed
+  get path(): string {
+    if (!this.parent) return "";
+    return this.parent.path + "/" + this.subpath;
+  }
+
+  @computed
+  get root(): INode {
+    let p;
+    let r = this;
+    while ((p = r.parent)) r = p;
+    return r as INode;
+  }
+}
+
+export function addTreeNodeAdministration(target) {
+  if (target[$treenode]) return target;
+
+  target[$treenode] = new TreeNode();
+  const treenodeProxyAPIs = {
+    parent: function getParent() {
+      return this[$treenode].parent.storedValue;
+    },
+
+    isRoot: function getIsRoot() {
+      return this[$treenode].isRoot;
+    },
+
+    path: function getPath() {
+      return this[$treenode].path;
+    },
+
+    root: function getRoot() {
+      return this[$treenode].root.storedValue;
     }
+  };
 
-    @computed
-    get path(): string {
-        if (!this.parent) return ""
-        return this.parent.path + "/" + this.subpath
-    }
+  Object.keys(treenodeProxyAPIs).forEach(key => {
+    Object.defineProperty(target, key, {
+      enumerable: false,
+      configurable: true,
+      get: treenodeProxyAPIs[key]
+    });
+  });
 
-    @computed
-    get root(): INode {
-        let p
-        let r = this
-        while ((p = r.parent)) r = p
-        return r as INode
-    }
+  self[$treenode].storedValue = target;
+  return target;
 }
